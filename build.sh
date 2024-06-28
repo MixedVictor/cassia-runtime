7caa2b53073b75071c785a180572629f516a8c9c
MixedVictor <victoraugusto.mixed@gmail.com>
2024-06-28 17:58:16 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env sh
 
-[[ -d "build" ]] && rm -rf build    || exit 1
-clear;cmake -GNinja -Bbuild -H. &&  \
-            cd build                || exit 1
-
-clear;cmake --build .               || exit 1
+[[ -d "build" ]] && rm -rf build
+clear;cmake -GNinja -Bbuild -H. && cd build || exit 1
+clear;cmake --build .                       || exit 1
