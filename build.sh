691b726c5c1666606a05103c3a011e041bd2581b
MixedVictor <victoraugusto.mixed@gmail.com>
2024-06-29 20:00:36 +0000

    Add flag to fix WINE compilation

--- build.sh
+++ build.sh
@@ -12,15 +12,16 @@ fi
 
 [[ -d "build" ]] && rm -rf build
 clear;cmake -GNinja -Bbuild -H.                                                             \
+            -DCMAKE_CXX_FLAGS="-Wno-error=incompatible-function-pointer-types"              \
             -DCMAKE_TOOLCHAIN_FILE=$NDK_PATH/build/cmake/android.toolchain.cmake            \
+            -DANDROID=ON                                                                    \
             -DANDROID_PLATFORM=android-29                                                   \
             -DANDROID_ABI=arm64-v8a                                                         \
             -DANDROID_SDK=$SDK_PATH                                                         \
             -DANDROID_NDK=$NDK_PATH                                                         \
-            -DANDROID=ON                                                                    \
             -DWINETOOLS_PATH=winetools                                                      \
             -DCASSIAEXT_BUILD_PATH=cassiaext                                                \
             -DMINGW_SYSROOT_PATH=/nix/store/4kkbq8p8p5cczlxzwgldwx9cvgddj73p-llvm-mingw/    \
-            -DDLL_BUILD_ARCHITECTURES=aarch64 &&                                            \
+            -DDLL_BUILD_ARCHITECTURES=i386 &&                                               \
             cd build    || exit 1
 clear;cmake --build .   || exit 1
