bbc257c6cfbcf9c02c0b347ca74dff23f71cddac
MixedVictor <victoraugusto.mixed@gmail.com>
2024-07-08 14:49:42 +0000

    Minor improvements

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
-### Cassiapp Runtime
+# Cassiapp Runtime
 
-#### Cloning
+## Cloning
 * You can clone this repository using `git clone --recursive` or cloning normally and then updating the submodules with `git submodule update --init --recursive`
 
-#### Building
+### But there is too many modules to clone and my internet can't handle it!
+
+Try running this repeatedly:
+```
+clear;git submodule deinit --force --all && git submodule update --init --recursive
+```
+
+## Building
 * Run `build.sh` to build Cassia's runtime
--- build.sh
+++ build.sh
@@ -4,9 +4,16 @@ SDK_PATH=/opt/android-sdk
 NDK_VER=26.3.11579264
 NDK_PATH=$SDK_PATH/ndk/$NDK_VER
 
-if [[ ! -d "winetools" ]]; then
-    mkdir winetools && cd winetools
-    ../wine/configure --enable-win64&&make -j$(nproc) || exit 1
+WINETOOLS_PATH=winetools
+CASSIAEXT_BUILD_PATH=cassiaext
+
+if [[ ! -d $WINETOOLS_PATH ]]; then
+    mkdir $WINETOOLS_PATH && cd $WINETOOLS_PATH
+    ../wine/configure --enable-win64&&make -j$(nproc) || {
+        cd ..
+        rm -rf $WINETOOLS_PATH
+        exit 1
+    }
     cd ..
 fi
 
@@ -19,9 +26,9 @@ clear;cmake -GNinja -Bbuild -H.                                                 
             -DANDROID_ABI=arm64-v8a                                                         \
             -DANDROID_SDK=$SDK_PATH                                                         \
             -DANDROID_NDK=$NDK_PATH                                                         \
-            -DWINETOOLS_PATH=winetools                                                      \
-            -DCASSIAEXT_BUILD_PATH=cassiaext                                                \
+            -DWINETOOLS_PATH=$WINETOOLS_PATH                                                \
+            -DCASSIAEXT_BUILD_PATH=$CASSIAEXT_BUILD_PATH                                    \
             -DMINGW_SYSROOT_PATH=/nix/store/4kkbq8p8p5cczlxzwgldwx9cvgddj73p-llvm-mingw/    \
-            -DDLL_BUILD_ARCHITECTURES=i386 &&                                               \
+            -DDLL_BUILD_ARCHITECTURES=i386,aarch64 &&                                       \
             cd build    || exit 1
 clear;cmake --build .   || exit 1
